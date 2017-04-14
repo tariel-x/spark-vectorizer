31fe89752cb31de42ef3770ff6d7e74fa4da3ace
Nikita Gerasimov <n.gerasimov@rambler-co.ru>
2017-04-14 18:54:40 +0300

    rewrite kmeans usage

--- run_corpus.py
+++ run_corpus.py
@@ -4,25 +4,26 @@ findspark.init("/home/nikita/bin/spark-2.1.0-bin-hadoop2.7")
 
 import sys
 from corpus_vectorizer import CorpusVectorizer
-from kmeans import KMeans
 from pyspark import SparkContext
+from numpy import array
+from math import sqrt
+from pyspark.mllib.clustering import KMeans, KMeansModel
 
-def process_articles(input_file, num_partitions=8):
-    sc = SparkContext()
-    try:
-        input_rdd = sc.textFile(input_file)
-        vectorized_docs = CorpusVectorizer(input_rdd).vectorize_corpus()
-        centroids       = KMeans(vectorized_docs).centroids
-        print >> sys.stdout, centroids.take(4)
-    except Exception as e:
-        print >> sys.stderr, "Unable to load file"
-        print >> sys.stderr, e
-        sys.exit(0)
+input_file = sys.argv[1]
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        input_file = sys.argv[1]
-        process_articles(input_file)
-    else:
-        print >> sys.stderr, "No file specified"
-        sys.exit(0)
+sc = SparkContext()
+input_rdd = sc.textFile(input_file)
+vectorized_docs = CorpusVectorizer(input_rdd).vectorize_corpus()
+clusters = KMeans.train(vectorized_docs, 2, maxIterations=10, initializationMode="random")
+
+
+def error(point):
+    center = clusters.centers[clusters.predict(point)]
+    return sqrt(sum([x**2 for x in (point - center)]))
+
+
+WSSSE = vectorized_docs.map(lambda point: error(point)).reduce(lambda x, y: x + y)
+print("Within Set Sum of Squared Error = " + str(WSSSE))
+
+clusters.save(sc, "KMeansModel")
+sameModel = KMeansModel.load(sc, "target/org/apache/spark/PythonKMeansExample/KMeansModel")
